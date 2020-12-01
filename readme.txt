7e90a7cd187ce391b1af11d4a9180551211b020d
Huzaifa <huzaifakhan771@gmail.com>
2020-12-01 19:48:14 +0500

    commiting all file for the project

---
+++ readme.txt
@@ -0,0 +1,6 @@
+Sequence to run this program
+
+1. Run detect_faces.py to detect and crop faces from images and save them as npz
+2. Run generate_embeddings.py to create embeddings for the faces and save them as npz
+3. Run train_SVM.py to train a classifier on the embeddings and save the model in pkl
+4. Run face_recog.py to read the pkl model and classify random images from the validation set
---
+++ train_SVM.py
@@ -0,0 +1,32 @@
+# develop a classifier for the 5 Celebrity Faces Dataset
+from random import choice
+from numpy import load
+from numpy import expand_dims
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import Normalizer
+from sklearn.svm import SVC
+from matplotlib import pyplot
+import pickle
+
+# load faces
+data = load('fb_dataset.npz')
+testX_faces = data['arr_2']
+# load face embeddings
+data = load('fb_dataset_embeddings.npz')
+trainX, trainy, testX, testy = data['arr_0'], data['arr_1'], data['arr_2'], data['arr_3']
+# normalize input vectors
+in_encoder = Normalizer(norm='l2')
+trainX = in_encoder.transform(trainX)
+testX = in_encoder.transform(testX)
+# label encode targets
+out_encoder = LabelEncoder()
+out_encoder.fit(trainy)
+trainy = out_encoder.transform(trainy)
+testy = out_encoder.transform(testy)
+# fit model
+model = SVC(kernel='linear', probability=True)
+model.fit(trainX, trainy)
+
+
+with open('fb_dataset_recog.pkl', 'wb') as classifier:
+    pickle.dump(model, classifier) 
